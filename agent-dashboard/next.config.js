db43af29a6b7145b492fca584b64ccd61f9a4c59
naji-navas <najidnavas2000@gmail.com>
2024-12-08 08:22:43 +0530

    updated the cinfig file

--- agent-dashboard/next.config.js
+++ agent-dashboard/next.config.js
@@ -5,6 +5,22 @@ const nextConfig = {
   images: {
     domains: ['assets.coingecko.com'],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader'],
+    });
+    return config;
+  },
+  // Enable CSS Modules
+  cssModules: true,
+  // Configure PostCSS
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 }
 
 module.exports = nextConfig 
